10a54ab392acf45e1be3a6c207096734fdb5d0ed
lucas <andrebastoslucas@gmail.com>
2024-09-27 18:47:19 -0300

    feat: home

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,13 @@ export default function Home() {
   return (
     <>
       <MainContentContainer>
-        <h1 className="font-bold text-6xl text-center mt-9">
-          Welcome to Voyager Backoffice!
-        </h1>
+        <div className="flex flex-col ">
+          <h1 className="font-bold text-6xl mt-9">
+            Bem vindo ao Voyager Backoffice!
+          </h1>
+
+          <h2 className="font-bold text-5xl mt-9">Visite nossa dashboard!</h2>
+        </div>
       </MainContentContainer>
     </>
   );
