7fbaf6f965e64fba034bd4bacabf70862361521f
lucas <andrebastoslucas@gmail.com>
2024-09-27 18:40:55 -0300

    feat: adicionando página home

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 "use client";
-import { useState } from "react";
-import Page from "./dashboard/page";
-import { Server } from "miragejs";
-import { mockEmployees } from "./mocks/mock-employees";
+
+import MainContentContainer from "./components/MainContentContainer";
 
 export default function Home() {
-  const [theme, setTheme] = useState("black");
   return (
     <>
-      <main className={`h-screen bg-bgprimary p-6 theme-${theme}`}></main>
+      <MainContentContainer>
+        <h1 className="font-bold text-6xl text-center mt-9">
+          Welcome to Voyager Backoffice!
+        </h1>
+      </MainContentContainer>
     </>
   );
 }
